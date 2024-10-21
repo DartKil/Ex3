831f79e83259609876d51e9d2d54a51773e8c546
TAN DAT <TAN DAT@DESKTOP-I56T031>
2024-10-21 23:13:24 +0700

    Add project files.

---
+++ WebApplication4/Models/EmailModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication4.Models
+{
+    public class EmailModel
+    {
+        public String From { get; set; }
+        public String To { get; set; }
+        public String Subject { get; set; }
+        public String Notes { get; set; }
+    }
+}
